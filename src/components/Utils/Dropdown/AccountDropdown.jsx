1d3ce3a9273c0b1ca5fe7d4f0beff5f796a3d839
25mohit <mohit724agarwal@gmail.com>
2024-08-15 01:26:26 +0530

    Dropdown Modal Developed Completly

--- src/components/Utils/Dropdown/AccountDropdown.jsx
+++ src/components/Utils/Dropdown/AccountDropdown.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AccountDropdown = () => {
   return (
-    <div className='account-option drop-md fdc'>
+    <div className='account-option drop-md fdc' onClick={e => e.stopPropagation()}>
         <div className="modal-overlay"></div>
         <p>Accounts Options</p>
     </div>
--- src/components/Utils/Dropdown/LanguageDropdown.jsx
+++ src/components/Utils/Dropdown/LanguageDropdown.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LanguageDropdown = () => {
   return (
-    <div className='language-dropdown drop-md fdc'>
+    <div className='language-dropdown drop-md fdc' onClick={e => e.stopPropagation()}>
         <div className="modal-overlay"></div>
         <p>Language Dropdown</p>
     </div>
