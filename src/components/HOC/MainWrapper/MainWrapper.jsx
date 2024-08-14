17c61dc5d20e524422c7c38e018dc17922aaa235
25mohit <mohit724agarwal@gmail.com>
2024-08-15 01:29:47 +0530

    Footer added

--- src/components/HOC/MainWrapper/MainWrapper.jsx
+++ src/components/HOC/MainWrapper/MainWrapper.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../../Utils/Parent/Navbar"
+import Footer from "../../Utils/Parent/Footer"
 
 const MainWrapper = () => {
   return (
@@ -8,6 +9,7 @@ const MainWrapper = () => {
       <section>
           <Outlet />
       </section>
+      <Footer />
     </>
   )
 }
